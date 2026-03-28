59f91aef19e63595841a361628592455a4d52748
Aarya3624 <rashinkaraarya@gmail.com>
2026-03-28 22:26:34 +0530

    Check

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://healthifys.com',
+  site: 'https://sanvi1232.github.io',
   base: '/healthify',
   vite: {
     plugins: [tailwindcss()]
